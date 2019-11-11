79add62f9d89a24645bb7053ab2e58aa63b3d2c8
Sekgomotso_s <motsoshalang@gmail.com>
2019-11-11 17:03:28 +0200

    Start-up

--- src/calculator.js
+++ src/calculator.js
@@ -21,8 +21,30 @@ function multiply() {
     return multiply;
 }
 
-class Add {
-    constructor (a,b) {
-        this.function = a + b;
+class Calculator {
+    constructor () {
+        this.add = 0;
+        this.multiply = 0;
+    }
+
+    // Add multiple numbers function
+    function add() {
+        var add = 0;
+        
+        for (i = 0; i < arguments.length; i++) {
+            this.add += arguments[i];
+        }
+        return add;
+    }
+
+    // Multiply multiple numbers function
+
+    function multiply() {
+        var multiply = 1;
+    
+        for (i = 1; i < arguments.length; i++) {
+            this.multiply *= arguments[i];
+        }
+        return multiply;
     }
 }
